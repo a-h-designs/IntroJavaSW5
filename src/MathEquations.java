68bf5b4b222b7defbcb8b6410fdff7d1499d1ffb
AHDesigns <anthony.hancock85@gmail.com>
2023-02-17 13:16:18 +0000

    Revert "Initial upload"
    This reverts commit 176f64fb347e5a95b1b31320c6b124af11caf809.

--- src/MathEquations.java
+++
@@ -1,8 +0,0 @@
-public class MathEquations { //Pascal Case naming convention for class name
-    public static void main(String[] args) { //shortcut 'main'
-        //'sout' to create the print statement
-        System.out.println("Java Math Equations");
-
-
-    }
-}
