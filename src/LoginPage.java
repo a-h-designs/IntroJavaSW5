ffa0a7ee9b2190e0eecf96c7d05fe0ef3f342d68
AHDesigns <anthony.hancock85@gmail.com>
2023-02-17 13:32:45 +0000

    Initial upload

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,2 +1,28 @@
+// this program will read user name (String) and password (int)
+
+import java.util.Scanner; // import Scanner class
+
 public class LoginPage {
+    public static void main(String[] args) {
+        // create Scanner object
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter you name : ");
+        // read username
+        String name = reader.nextLine();
+
+
+        System.out.println("Enter your age : ");
+        // read password
+        int age = reader.nextInt();
+
+        System.out.println("Enter you location : ");
+        // read username
+        String location = reader.nextLine();
+
+        // print information
+        System.out.println("Your name is " + name);
+        System.out.println("Your age is " + age);
+        System.out.println("Your location is " + location);
+    }
 }
